87dac1e92abb8f96c209bb98f71d4ccb5c683fa0
user <anna_prokopas@mail.ru>
2019-08-16 00:46:24 +1000

    add config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-# ... add more variables here as needed
+app.config.from_object(Config)
 
 from app import routes
