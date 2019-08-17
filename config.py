41d5b7c35a1666c0ed22dc51900c39a67066fd66
user <anna_prokopas@mail.ru>
2019-08-17 22:51:11 +1000

    add db Users (SQLite)

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # отключить функцию Flask-SQLAlchemy, которая мне не нужна, которая должна сигнализировать 
+    # приложению каждый раз, когда в базе данных должно быть внесено изменение
