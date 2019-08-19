1b41e7a13363bfb806fc738308b8096d8690564f
user <anna_prokopas@mail.ru>
2019-08-19 11:31:47 +1000

    use db and flask shell

--- microblog.py
+++ microblog.py
@@ -1,4 +1,11 @@
-from app import app
+from app import app, db
+import os
+from app.models import User, Post
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
